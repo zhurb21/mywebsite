8fe66f1965829c60c0eb5707e8c9f61f7d6dd6d5
zhurb21 <nickzhurbich23@gmail.com>
2020-10-19 13:31:28 +0700

    7 commit

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("its working")
+
+let theme = localStorage.getItem('theme-dot')
+
+if(theme == null){
+    setTheme('light')
+}else{
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i = 0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode
+        console.log("Option Clicked:", mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if(mode == 'light'){
+        document.getElementById('theme-style').href = "default.css"
+    }
+
+    if(mode == 'blue'){
+        document.getElementById('theme-style').href = "blue.css"
+    }
+
+    if(mode == 'green'){
+        document.getElementById('theme-style').href = "green.css"
+    }
+
+    if(mode == 'orange'){
+        document.getElementById('theme-style').href = "orange.css"
+    }
+
+    localStorage.setItem('theme', mode)
+}
